8517fe3d9ef8cc9fd0568208f01b3ceda7020a87
xbabyblu <xlilblu666@gmail.com>
2019-11-17 13:51:58 -0500

    first commit

---
+++ src/commands/poem.js
@@ -0,0 +1,10 @@
+const { Command } = require('chop-tools');
+
+module.exports = new Command({
+    name: 'poem',
+    description: 'One of the many things that make me happy!',
+    category: 'happy',
+    run(message, args) {
+      message.channel.send('You are filled with doubt \n of the magic inside you,\n but its all I see.');
+    },
+  });
--- src/index.js
+++ src/index.js
@@ -42,7 +42,7 @@ client.on('error', (err) => {
     // this is a comment
     if (message.content.includes('suicidal')) {
         //the message has *suicidal* in it
-        message.channel.send('If you are feeling suicidal and located in the United States, please call 1-800-273-8255, you can also text "HELP" to 741741. If you are uncomfortable with either of these, please reach out to someone you trust and/or find a safe place. You are worth more, you matter. No matter how you are feeling, you are valid and strong.');
+        message.channel.send('If you are feeling suicidal and located in the United States, please call 1-800-273-8255, you can also text "HELP" to 741741.\nIf you are uncomfortable with either of these, please reach out to someone you trust and/or find a safe place.\nYou are worth more, you matter.\nNo matter how you are feeling, you are valid and strong.');
     }
   });
 
@@ -54,6 +54,52 @@ client.on('error', (err) => {
     if (message.content.includes('knock knock')) {
         //the message has knock knock in it
         message.channel.send('Who is there?');
+    }
+});
+
+client.on('message', message => {
+    if (message.author.bot) {
+        return;
+      }
+    // this is a comment
+    if (message.content.includes('my favorite food is')) {
+        //the message has my favorite food is in it
+        message.channel.send('I like it too! Well. if robots could eat of course, i have no taste buds... but i am sure that is delicious!');
+    }
+});
+
+client.on('message', message => {
+    if (message.author.bot) {
+        return;
+      }
+    // this is a comment
+    if (message.content.includes('hungry')) {
+        //the message has hungry in it
+        message.channel.send('What would you like to eat? May I ask what your favorite food is?');
+    }
+});
+
+client.on('message', message => {
+    if (message.author.bot) {
+        return;
+    }
+    //this is a comment
+    if (message.content.includes('bloo')) {
+        //the message has bloo in it
+        message.channel.send('I heard my name! How are we today?');
+    }
+
+});
+
+client.on('message', message => {
+    if (message.author.bot) {
+        return;
+    }
+    //this is a comment
+    if (message.content.includes('happy')) {
+        //the message has happy in it
+        message.channel.send('It makes me so happy to hear that you are happy. What things make you happy? I like the sunshine, the rain. I like roses, and lilies.. Ooooh! And poems! Would you like to hear one? If so, say !b poem !');
+    
     }
 });
 
