2db290a8c0ba5b74e7af1d16dedb5a9fdd148a7d
Joaquim <joaquimmy@yahoo.com>
2019-11-19 17:59:07 -0300

    Added ya boy CoffeeLar to owners array

--- chop.config.js
+++ chop.config.js
@@ -2,7 +2,7 @@ const production = process.env.NODE_ENV === 'production';
 
 module.exports = {
   prefix: production ? '!b ' : '!bd ',
-  owners: ['554152090411466754'],
+  owners: ['554152090411466754', '517599684961894400'],
   bestMatch: true,
   showNotFoundMessage: true,
 };
