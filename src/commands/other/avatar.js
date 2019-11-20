f8c6be66ceadc1b98b80e5a3d968c531cc676d75
Joaquim <joaquimmy@yahoo.com>
2019-11-19 22:53:09 -0300

    Add avatar command

---
+++ src/commands/other/avatar.js
@@ -0,0 +1,21 @@
+const { Command } = require('chop-tools');
+const { MessageEmbed } = require('discord.js');
+
+const makeEmbed = require('../../util/makeEmbed');
+
+module.exports = new Command({
+  name: 'avatar',
+  description: "Displays someone's avatar.",
+  args: ['target'],
+  run(message) {
+    const target = message.mentions.members.first();
+    if (!target) {
+      message.channel.send("I couldn't find that person.");
+      return;
+    }
+
+    message.channel.send({
+      embed: makeEmbed(target.user.tag + "'s Avatar", target.user.displayAvatarURL({ size: 512 }), message),
+    });
+  },
+});
