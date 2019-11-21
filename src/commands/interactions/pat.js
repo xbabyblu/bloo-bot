2ae1bd7c14176523dfeccb808911885e4967bb82
xbabyblu <xlilblu666@gmail.com>
2019-11-20 19:01:31 -0500

    added gifs to interactions

--- src/commands/interactions/pat.js
+++ src/commands/interactions/pat.js
@@ -1,5 +1,19 @@
 const { Command } = require('chop-tools');
 
+const makeEmbed = require('../../util/makeEmbed');
+const random = require('../../util/random');
+const findPerson = require('../../util/findPerson');
+
+const images = [
+  'https://imgur.com/a/DThVg90',
+  'https://imgur.com/a/dhxewzW',
+  'https://imgur.com/a/cnHtlCn',
+  'https://imgur.com/a/uqxcpQN',
+  'https://imgur.com/a/A5wp6YO',
+  'https://imgur.com/a/XwcZRJQ',
+  'https://imgur.com/a/ZAfS394',
+];
+
 module.exports = new Command({
   name: 'PatPat',
   description: 'a gentle way of saying.. there-there.',
@@ -7,14 +21,21 @@ module.exports = new Command({
   args: ['target'],
   delete: true,
   category: 'interactions',
-  run(message, args, call) {
-    const target = message.mentions.members.first();
+  async run(message, args, call) {
+    const target = await findPerson(message.mentions.members.first());
     if (!target) {
       message.channel.send("I couldn't find that person.");
       return;
     }
-    target.user.send(
-      `*pat-pat* \n${call.callerTag} has pat you c: `,
+
+    const embed = makeEmbed(
+      `*pat-pat* \n${call.callerTag} has pat you c:`,
+      random(images),
+      message,
     );
+
+    target.send({ embed });
   },
 });
+// need to put in embed for gifs 
+// tysm
--- src/commands/interactions/poke.js
+++ src/commands/interactions/poke.js
@@ -1,19 +1,32 @@
 const { Command } = require('chop-tools');
 
+const makeEmbed = require('../../util/makeEmbed');
+const random = require('../../util/random');
+const findPerson = require('../../util/findPerson');
+
+const images = [
+  '',
+];
+
 module.exports = new Command({
   name: 'poke',
   description: 'poke a cutie',
   args: ['target'],
   delete: true,
   category: 'interactions',
-  run(message, args, call) {
-    const target = message.mentions.members.first();
+  async run(message, args, call) {
+    const target = await findPerson(message.mentions.members.first());
     if (!target) {
       message.channel.send("I couldn't find that person.");
       return;
     }
-    target.user.send(
-      `\n${call.callerTag} poked you :yum:`,
+
+    const embed = makeEmbed(
+      `${call.callerTag} poked you :yum:`,
+      random(images),
+      message,
     );
+
+    target.send({ embed });
   },
 });
