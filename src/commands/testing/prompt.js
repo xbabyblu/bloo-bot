e462f489fbc60976671de62d0b3af863c924c49e
Joaquim <joaquimmy@yahoo.com>
2019-11-22 19:11:39 -0300

    Add prompt test command

---
+++ src/commands/testing/prompt.js
@@ -0,0 +1,100 @@
+const { Command } = require('chop-tools');
+const Prompter = require('chop-prompter');
+const request = require('request-promise-native');
+
+const listen = require('../../util/listen');
+
+module.exports = new Command({
+  name: 'prompt',
+  description: 'Prompting test',
+  category: 'testing',
+  hidden: true,
+  run(message, args, call) {
+    if (args[0] && args[0] === 'reaction') {
+      Prompter.confirm({
+        channel: message.channel,
+        timeout: 5000,
+        userId: message.author.id,
+        confirmEmoji: '💓',
+        cancelEmoji: '💔',
+      })
+        .then(res => {
+          // console.log(res);
+          message.channel.send(res ? 'Confirmed!' : 'Cancelled :c');
+        })
+        .catch(err => {
+          console.log(err);
+          message.channel.send('Error.');
+        });
+      return;
+    }
+
+    if (args[0] && args[0] === 'test') {
+      Prompter.message({
+        channel: message.channel,
+        question: 'What is Lar\'s favorite food?',
+        userId: call.caller,
+        deleteMessage: false,
+        regex: /spaghetti/gi,
+        max: 1,
+      })
+        .then(res => {
+          if (!res) return message.channel.send(':c');
+          message.channel.send(`Yes ${call.callerUsername}, you got it!`);
+        })
+        .catch((err) => {
+          console.log(err);
+          message.channel.send(':c');
+        });
+      return;
+    }
+
+    Prompter.message({
+      channel: message.channel,
+      question: 'Do you want to hear a joke?',
+      // prefix: 'I am',
+      deleteMessage: false,
+    })
+      .then(userResponse => {
+        if (listen(userResponse.first(), ['{yes}'])) {
+          request('https://icanhazdadjoke.com/', {
+            headers: {
+              accept: 'application/json',
+              'User-Agent': 'Bloo Bot (https://github.com/xbabyblu/Bloo-Bot)',
+            },
+          })
+            .then(jokeResponse => {
+              const { joke } = JSON.parse(jokeResponse);
+              message.channel
+                .send({
+                  embed: {
+                    title: joke,
+                    description: ':rofl:',
+                    thumbnail: {
+                      url: this.client.user.avatarURL(),
+                    },
+                  },
+                })
+                .catch(() => {});
+            })
+            .catch(() => {
+              message.channel.send({
+                embed: {
+                  title: 'Knock Knock. Who is there? Oh its me! Bloo, duh! ~ ',
+                  description: ':rofl:',
+                  thumbnail: {
+                    url: this.client.user.avatarURL(),
+                  },
+                },
+              }).catch(() => {});
+            });
+        } else {
+          message.channel.send('Okay then.');
+        }
+      })
+      .catch(err => {
+        console.log(err);
+        message.channel.send('Okay then.');
+      });
+  },
+});
