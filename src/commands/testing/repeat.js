e2daee538adf7ab2b31293e1b02aadc7ef4b9c6b
Joaquim <joaquimmy@yahoo.com>
2019-11-23 20:27:56 -0300

    Better logging

--- src/commands/testing/repeat.js
+++ src/commands/testing/repeat.js
@@ -4,8 +4,10 @@ module.exports = new Command({
   name: 'repeat',
   description: 'Repeats stuff like a robot',
   category: 'funny',
-  run(message, args) {
+  run(message, args, call) {
+    console.log(call.callerTag, message.content);
+    console.log(Reflect.ownKeys(call));
     message.channel.send('args: ' + args);
-    message.channel.send(message.author + ' said ' + args.join(' '));
+    message.channel.send(message.author.username + ' said ' + args.join(' '));
   },
 });
