dc839fd301768d0ec896edbcb7766e3bb16f41ef
xbabyblu <xlilblu666@gmail.com>
2019-11-25 19:37:26 -0500

    added knock knock listeners/responses
    Co-authored-by: Joaquim Neto <joaquimmy@yahoo.com>

---
+++ src/listeners/misc/knockknock.js
@@ -0,0 +1,51 @@
+const { Listener } = require('chop-tools');
+
+const Prompter = require('chop-prompter');
+
+module.exports = new Listener({
+  words: 'knock knock',
+  category: 'misc',
+  cooldown: 1,
+  priority: 5,
+  async run(message) {
+    const responseList1 = await Prompter.message({
+      channel: message.channel,
+      question: 'Who\'s there?',
+      userId: message.author.id,
+      max: 1,
+      timeout: 10000,
+    });
+
+    // If no responses, the time ran out
+    if (!responseList1) {
+      // i sure hope its not Joe lmfao xD || this is a cry for help
+      message.channel.send('....*Who\'s there?*');
+      return true;
+    }
+    
+    const response1 = responseList1.first();
+
+    // you can put this response1 👈👈👈👈👈👈
+    await message.channel.send(`${response1} who?`);
+
+    const responseList2 = await Prompter.message({
+      channel: message.channel,
+      // in here   \/   👈👈👈👈👈👈
+      question: ':nerd:',
+      userId: message.author.id,
+      max: 1,
+      timeout: 10000,
+    });
+    // i didn't think this far
+    if (!responseList2) {
+      await message.channel.send('***bruh***');
+      return true;
+    }
+
+    const response2 = responseList2.first();
+
+    await message.channel.send(`*${response2}*... the heck!?`);
+    
+    return true;
+  },
+});
