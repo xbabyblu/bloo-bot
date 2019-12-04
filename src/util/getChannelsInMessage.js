bdeed2c18a0271cf54d20412c7200f2b4a9a118e
Joaquim <joaquimmy@yahoo.com>
2019-12-04 09:43:12 -0300

    Add helper function to get channels in a message

---
+++ src/util/getChannelsInMessage.js
@@ -0,0 +1,17 @@
+const channelRegex = /(?<=<#)(\d+?)(?=>)/g;
+
+module.exports = message => {
+  const channelsInMessage = message.content.match(channelRegex) || [];
+
+  const channelsInGuild = message.guild.channels.filter(c => c.type === 'text');
+
+  const channels = channelsInMessage
+    // remove duplicates
+    .filter((v, i, a) => a.indexOf(v) === i)
+    // get the channels
+    .map(c => channelsInGuild.get(c))
+    // remove falsy values
+    .filter(c => !!c);
+
+  return channels;
+};
