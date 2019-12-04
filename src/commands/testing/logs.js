35d152354376f7efd07a73708f91445f2ba6e2b8
Joaquim <joaquimmy@yahoo.com>
2019-12-04 09:49:07 -0300

    Command to set logs channel

---
+++ src/commands/testing/logs.js
@@ -0,0 +1,27 @@
+const { Command } = require('chop-tools');
+
+const Bloo = require('../../models/bloo');
+const getChannelsInMessage = require('../../util/getChannelsInMessage');
+
+module.exports = new Command({
+  name: 'logs',
+  description: 'Set the logs channel for Bloo globally. (OWNER ONLY)',
+  category: 'admin',
+  aliases: ['log'],
+  hidden: true,
+  async run(message, args, call) {
+    let [channel] = getChannelsInMessage(message);
+    // configure the channel
+    const blooConfig = await Bloo.findOne({}).exec();
+    if (channel) {
+      blooConfig.logs.channel = channel.id;
+      blooConfig.logs.guild = channel.guild.id;
+    } else {
+      blooConfig.logs.channel = message.channel.id;
+      blooConfig.logs.guild = message.guild.id;
+      channel = message.channel;
+    }
+    await blooConfig.save()
+    message.channel.send(`Logs channel set to: ${channel}`);
+  },
+});
