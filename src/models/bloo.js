fcaeceb49d29edc9111e1b5343135a6d4d7e5b73
Joaquim <joaquimmy@yahoo.com>
2019-12-05 08:39:01 -0300

    Add logic to give ideas unique ids

--- src/models/bloo.js
+++ src/models/bloo.js
@@ -19,6 +19,36 @@ const blooSchema = new Schema({
       type: String,
     },
   },
+  stats: {
+    currentIdeaId: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now(),
+  },
+  updatedAt: {
+    type: Date,
+    required: true,
+    default: Date.now(),
+  }
+});
+
+blooSchema.pre('save', function preSave(next) {
+  if (this.isModified('createdAt')) {
+    throw new Error('Creation field is read only!');
+  } else {
+    next();
+  }
+});
+
+blooSchema.pre('save', function(next) {
+  this.updatedAt = Date.now();
+  next();
 });
 
 module.exports = mongoose.model('Bloo', blooSchema);
--- src/models/idea.js
+++ src/models/idea.js
@@ -1,8 +1,15 @@
 const mongoose = require('mongoose');
 
+const Bloo = require('./bloo');
+
 const { Schema } = mongoose;
 
 const ideaSchema = new Schema({
+  ideaId: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
   title: {
     type: String,
     minlength: 3,
@@ -10,7 +17,7 @@ const ideaSchema = new Schema({
   },
   creator: {
     type: String,
-    required: true
+    required: true,
   },
   isDone: {
     type: Boolean,
@@ -25,21 +32,23 @@ const ideaSchema = new Schema({
     type: Date,
     required: true,
     default: Date.now(),
-  }
+  },
 });
 
-
-ideaSchema.pre('save', function preSave(next) {
+ideaSchema.pre('save', async function preSave(next) {
   if (this.isModified('createdAt')) {
     throw new Error('Creation field is read only!');
-  } else {
-    next();
   }
-});
-
-ideaSchema.pre('save', function preSave(next) {
   this.updatedAt = Date.now();
+  // get next id
+  const blooConfig = await Bloo.findOne({}).select('stats.currentIdeaId').exec();
+  const nextId = blooConfig.stats.currentIdeaId + 1;
+  this.ideaId = nextId;
+  blooConfig.stats.currentIdeaId += 1;
+  await blooConfig.save();
   next();
 });
+
+// TODO: Add mongoose-unique-validator
 
 module.exports = mongoose.model('Idea', ideaSchema);
