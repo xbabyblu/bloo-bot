11064fe2cc5830ad9548c87e9a97b20683d67113
Joaquim <joaquimmy@yahoo.com>
2019-12-09 20:19:42 -0300

    Remove time from log message

--- src/services/logCommands.js
+++ src/services/logCommands.js
@@ -1,8 +1,6 @@
 const logger = require('./logger');
 
 module.exports = (call, next) => {
-  logger.debug(
-    `[${new Date().toLocaleTimeString()}] ${call.callerTag}: ${call.message.content} | (${call.message.guild.name})`,
-  );
+  logger.debug(`${call.callerTag}: ${call.message.content} | (${call.message.guild.name})`);
   next();
 };
