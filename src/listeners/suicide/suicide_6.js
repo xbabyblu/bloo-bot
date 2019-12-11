d0dc5db1eaae1bed8869e1aeea4447fb86cf2631
xbabyblu <xlilblu666@gmail.com>
2019-12-11 13:47:59 -0500

    added suicide hotlines all around the world.

--- src/listeners/suicide/suicide_6.js
+++ src/listeners/suicide/suicide_6.js
@@ -2,7 +2,7 @@ const { Listener } = require('chop-tools');
 
 const sentiment = require('../../services/sentiment');
 
-const MSG = 'If you are feeling suicidal and located in the United States, please call 1-800-273-8255, you can also text "HELP" to 741741.\nIf you are uncomfortable with either of these, please reach out to someone you trust and/or find a safe place.\nYou are worth more, you matter.\nNo matter how you are feeling, you are valid and strong.';
+const MSG = 'If you are feeling suicidal, please call the number in your area listed below. If you are uncomfortable calling, please reach out to someone you trust and/or find a safe place. You are worth more, you matter. Now matter how you are feeling, you are valid and strong. \n1-800-273-8255 United States\n0845 790 9090 United Kingdom\n1833 456 4566 Canada\n0145 394 000 France\n0800 181 0771 Germany\n13 11 14 Australia\n888 8817 666 India\n525 510 2550 Mexico\n+810 352 869 090 Japan\n914 590 050 Spain\n051 444 5691 South Africa\n0800 543 354 New Zealand';
 
 module.exports = new Listener({
   words: ['{me}', '(want|wanna|gonna|going to)', '(off|kill)', 'myself'],
