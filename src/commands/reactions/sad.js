34fc5c4ea96141424de9ef07253f966c4a2062f1
xbabyblu <xlilblu666@gmail.com>
2019-12-11 19:51:28 -0500

    added gifs to happy/hungry/sad

--- src/commands/reactions/sad.js
+++ src/commands/reactions/sad.js
@@ -1,16 +1,4 @@
-const { Command } = require('chop-tools');
 
-module.exports = new Command({
-  name: 'Sad',
-  description: 'i am here for you!',
-  category: 'reactions',
-  hidden: true,
-  run(message) {
-    message.channel.send('Do you want to talk about it?');
-  },
-});
-
-/*
 const { Command } = require('chop-tools');
 
 const makeEmbed = require('../../util/makeEmbed');
@@ -18,12 +6,30 @@ const random = require('../../util/random');
 const findPerson = require('../../util/findPerson');
 const format = require('../../util/format');
 
-const images = [];
+const images = [
+  'https://imgur.com/a/uGONoUP',
+  'https://imgur.com/a/jZywU2t',
+  'https://imgur.com/a/Qhx6F0F',
+  'https://imgur.com/a/U9bvC7v',
+  'https://imgur.com/a/lt7ApGG',
+  'https://imgur.com/a/DyGvW9n',
+  'https://imgur.com/a/tITzTR9',
+  'https://imgur.com/a/Rzicel8',
+  'https://imgur.com/a/QrxhdS6',
+  'https://imgur.com/a/KEiX1le',
+  'https://imgur.com/a/Q9KvtGd',
+  'https://imgur.com/a/jz5FYkb',
+  'https://imgur.com/a/4YISJ0a',
+  'https://imgur.com/a/00V94WF',
+  'https://imgur.com/a/kJORNI9',
+  'https://imgur.com/a/JUHUwq8',
+];
 
+/*
 for (let i = 0; i <= 28; i++) {
   images.push(`http://cdn.chop.coffee/happy/${i}.gif`);
 }
-
+*/
 module.exports = new Command({
   name: 'sad',
   description: "don't cry... things will get better :frown:",
@@ -38,7 +44,7 @@ module.exports = new Command({
     if (target) {
       msg = `<@${call.caller}> is crying because of ${target}. Why would you do that to them? :c`;
     } else {
-      msg = `<@${call.caller}> is sad, someone should try to cheer them up.. :crying: `;
+      msg = `<@${call.caller}> is sad, someone should try to cheer them up.. :sob: `;
     }
 
     const embed = makeEmbed(msg, random(images), message);
@@ -46,4 +52,3 @@ module.exports = new Command({
     message.channel.send({ embed });
   },
 });
-*/
