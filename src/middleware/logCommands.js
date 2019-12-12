d5ae099002f75ed5eee6d1a5a197a57fba2ab2d5
Joaquim <joaquimmy@yahoo.com>
2019-12-12 19:45:15 -0300

    Fix import

--- src/middleware/logCommands.js
+++ src/middleware/logCommands.js
@@ -1,4 +1,4 @@
-const logger = require('./logger');
+const logger = require('../services/logger');
 
 module.exports = (call, next) => {
   logger.debug(`${call.callerTag}: ${call.message.content} | (${call.message.guild.name})`);
