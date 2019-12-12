df846e8ae00c390493dd966cef35d30f4eaf6214
Joaquim <joaquimmy@yahoo.com>
2019-12-12 19:53:29 -0300

    Sentiment IIFE

--- src/services/sentiment.js
+++ src/services/sentiment.js
@@ -1,22 +1,30 @@
-const { Text } = require("chop-tools");
-const multilangSentiment = require("multilang-sentiment");
+const multilangSentiment = require('multilang-sentiment');
 
 const words = {
-  "🍥": 0,
-  "🍱": 0,
-  "❤️": 2,
-  "🙂": 1,
+  '🍥': 0,
+  '🍱': 0,
+  '❤️': 2,
+  '🙂': 1,
   // hang: -5,
   // depressed: -5,
   // 'yeet': -5,
-  'kms': -5 // for suicide_10.js
+  kms: -5, // for suicide_10.js
 };
 
-// Sentiment IIFE to save a reference to the client.
-const sentiment = text => {
-  const result = multilangSentiment(text, "en", { words });
-  // console.log(`Sentiment: ${text} -> ${result.score}`)
-  return result;
-};
+// Sentiment IIFE to save a reference to the client and log sentiment usage.
+const sentiment = (() => {
+  let client;
+  const S = text => {
+    const result = multilangSentiment(text, 'en', { words });
+    if (client) {
+      client.emit('sentiment', { length: text.length, score: result.score });
+    }
+    return result;
+  };
+  S.setClient = clt => {
+    client = clt;
+  };
+  return S;
+})();
 
 module.exports = sentiment;
