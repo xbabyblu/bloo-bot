90a748337df8ea07d509d774b33d6e8fb7e1ddf3
Joaquim <joaquimmy@yahoo.com>
2019-12-12 19:57:44 -0300

    Bump priority to 20

--- src/listeners/bloo/beMyGf.js
+++ src/listeners/bloo/beMyGf.js
@@ -4,7 +4,7 @@ module.exports = new Listener({
   words: ['bloo', '{be}', 'my', '(gf|girlfriend)'],
   category: 'bloo',
   cooldown: 10,
-  priority: 0,
+  priority: 20,
   run(message) {
     this.send("I'm flattered... But I'm underage.");
     return false;
