c63709289ec41dc97d2612bdda94b729ae8329ef
Joaquim <joaquimmy@yahoo.com>
2019-12-12 20:00:55 -0300

    Should not go to next listener

--- src/listeners/bloo/beMyGf.js
+++ src/listeners/bloo/beMyGf.js
@@ -4,9 +4,9 @@ module.exports = new Listener({
   words: ['bloo', '{be}', 'my', '(gf|girlfriend)'],
   category: 'bloo',
   cooldown: 10,
-  priority: 20,
+  priority: 0,
   run(message) {
     this.send("I'm flattered... But I'm underage.");
-    return false;
+    return true;
   },
 });
