91b6dc7e8c98b070dceb5126727d6ed44eba630f
Joaquim <joaquimmy@yahoo.com>
2019-12-17 20:38:10 -0300

    brb command

--- src/commands/other/berightback.js
+++ src/commands/other/berightback.js
@@ -1,4 +1,4 @@
-const { Command } = require('chop-tools');
+const { Command, Text } = require('chop-tools');
 const Prompter = require('chop-prompter');
 
 const promptForNewBrbMessage = async (message, args, call, send) => {
@@ -19,20 +19,21 @@ const promptForNewBrbMessage = async (message, args, call, send) => {
   call.profile.brb = brb;
   await call.profile.save();
   send('Done, brb message saved!');
-}
+};
 
 module.exports = new Command({
   name: 'berightback',
-  description: 'For when you just have to go :P',
+  description: Text.lines('For when you just have to go :P', 'Use it when you gotta brb ;)'),
   category: 'other',
   aliases: ['beback'],
+  examples: [' ', 'edit'],
   delete: true,
   async run(message, args, call) {
     const currentBrb = call.profile.brb;
     if (!currentBrb) {
       const response = await Prompter.confirm({
         channel: message.channel,
-        question: 'You don\'t have a brb message yet, would you like to set one?',
+        question: "You don't have a brb message yet, would you like to set one?",
         userId: call.caller,
       });
 
@@ -41,7 +42,7 @@ module.exports = new Command({
         return;
       }
 
-      await promptForNewBrbMessage(message,args,call, this.send);
+      await promptForNewBrbMessage(message, args, call, this.send);
       return;
     }
 
