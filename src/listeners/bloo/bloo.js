13b4bc054deba02c972374b1d4674a35534aa1f1
xbabyblu <xlilblu666@gmail.com>
2019-12-23 00:02:31 -0500

    bloo only listens to her name every 30 minutes

--- src/listeners/bloo/bloo.js
+++ src/listeners/bloo/bloo.js
@@ -6,6 +6,7 @@ module.exports = new Listener({
   words: 'bloo',
   category: 'bloo',
   cooldown: 1800000,
+  // 30 minutes
   priority: 10,
   run(message) {
     send(message)('I heard my name! How are we today?');
