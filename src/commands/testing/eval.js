dc16d6d522f33affa139c48803965f53774fe410
Joaquim <joaquimmy@yahoo.com>
2020-01-10 23:56:40 -0300

    Improve error handling in eval command

--- src/commands/testing/eval.js
+++ src/commands/testing/eval.js
@@ -1,17 +1,26 @@
-const { Command } = require("chop-tools");
+const { Command } = require('chop-tools');
 
 module.exports = new Command({
-  name: "eval",
-  description: "eval stuff",
-  category: "testing",
+  name: 'eval',
+  description: 'eval stuff',
+  category: 'testing',
   hidden: true,
   async run(message, args, call) {
+    const handleError = err => this.send('Oopsie!\n' + err.message);
+
     // lets double check just to be sure, haha
-    if (message.author.id in this.client.options.owners) {
-      // eslint-disable-next-line no-eval
-      const result = eval(call.content);
-      message.channel.send("Done.");
-      message.channel.send(result).catch(() => {/* meh */});
+    if (call.isSuperUser) {
+      try {
+        // eslint-disable-next-line no-eval
+        const result = eval(call.content);
+        let msg = 'Done.';
+        if (result) {
+          msg += 'Result: ```' + String(result) + '```';
+        }
+        message.channel.send(msg).catch(handleError);
+      } catch (err) {
+        handleError(err);
+      }
     }
-  }
+  },
 });
