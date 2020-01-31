87162f2c81d1975824769038a5c78c410fee5758
Joaquim <joaquimmy@yahoo.com>
2020-01-31 11:04:24 -0300

    Improve vote event error handling and message

--- src/commands/info/voting.js
+++ src/commands/info/voting.js
@@ -8,7 +8,8 @@ module.exports = new Command({
   run() {
     this.send(
       "I'm so **happy** you want to vote for me! I promise I'll remember this~ 💙",
-      "You can vote for me every 12 hours. c:",
+      'You can vote for me every 12 hours. c:',
+      'During weekends voting gives you __double rewards__!',
       'https://top.gg/bot/643338599281983501/vote',
     );
   },
--- src/events/vote.js
+++ src/events/vote.js
@@ -1,3 +1,5 @@
+const { Random } = require('chop-tools');
+
 const Profile = require('../models/profile');
 const Currency = require('../services/currency');
 const Alert = require('../services/alert');
@@ -8,42 +10,118 @@ const { VOTING_REWARD_NORMAL, VOTING_REWARD_WEEKEND } = require('../BLOO_GLOBALS
 module.exports = client => async vote => {
   client.logger.debug('[Event] New vote! ->', vote);
 
-  // FIXME: lazy try catch
+  let profile;
   try {
-    const profile = await Profile.getOrCreate(vote.user).catch(err => client.emit('error', err));
+    profile = await Profile.getOrCreate(vote.user);
+  } catch (err) {
+    err.message += `\nCould not get profile for registering a vote for user ${vote.user}.`;
+    client.emit('error', err);
+    return;
+  }
 
-    const amount = vote.isWeekend ? VOTING_REWARD_WEEKEND : VOTING_REWARD_NORMAL;
-    const newBalance = await Currency.add(vote.user, amount);
-
-    profile.votes.count += 1;
-    profile.votes.time = Date.now();
-    await profile.save();
-
-    // TODO: Also check profiles on database
-    const user = client.users.get(vote.user);
-
-    if (!user) {
-      Alert.log(Alert.types.vote, client, `Someone Bloo has never seen before just voted for her! Yeehaw!`).catch(err =>
-        client.emit('err', err),
-      );
-      return;
+  const amount = vote.isWeekend ? VOTING_REWARD_WEEKEND : VOTING_REWARD_NORMAL;
+  let newBalance;
+  try {
+    if (process.env.NODE_ENV === 'production') {
+      newBalance = await Currency.add(vote.user, amount);
+    } else {
+      newBalance = profile.money;
     }
+  } catch (err) {
+    err.message += `\nCould not give voting currency to ${vote.user}.`;
+    client.emit('error', err);
+    return;
+  }
 
+  profile.votes.count += 1;
+  profile.votes.time = Date.now();
+  try {
+    if (process.env.NODE_ENV === 'production') {
+      await profile.save();
+    }
+  } catch (err) {
+    err.message += `\nCould not save profile for user ${vote.user} after voting.`;
+    client.emit('error', err);
+    return;
+  }
+
+  // TODO: Also check profiles on database
+  const user = client.users.get(vote.user);
+
+  if (!user) {
+    try {
+      Alert.log(
+        Alert.types.vote,
+        client,
+        `Someone Bloo doesn't share a server with just voted for her! Yeehaw!`,
+      );
+    } catch (err) {
+      err.message += `\nFailed to send log message to bloo dev server... WTF?`;
+      client.emit('error', err);
+    }
+    return;
+  }
+
+  try {
     Alert.log(Alert.types.vote, client, `${user.tag} just voted for us! Yeehaw!`, {
       thumbnail: user.displayAvatarURL({ size: 512 }),
-    }).catch(err => client.emit('err', err));
-
-    user
-      .send(
-        format(
-          '>///< Thank you so so much for voting for me :heart:',
-          `I'm giving you **${amount}${INK_EMOJI}** for this!`,
-          `You now have **${newBalance}${INK_EMOJI}**`,
-        ),
-      )
-      // Could not dm user. Sad but ok.
-      .catch(() => {});
+    });
   } catch (err) {
+    err.message += `\nFailed to send log message to bloo dev server... WTF?`;
     client.emit('error', err);
   }
+
+  const faces = [
+    '(✿◠‿◠)',
+    '≧◡≦',
+    '(●´ω｀●)',
+    '(ﾉ◕ヮ◕)ﾉ*:･ﾟ✧',
+    '(づ｡◕‿‿◕｡)づ',
+    '(•⊙ω⊙•)',
+    '(∪ ◡ ∪)',
+    '◕ ◡ ◕',
+    'ヽ(゜∇゜)ノ',
+    '(⌒o⌒)',
+    '(◡‿◡✿)',
+    '(✿ ♥‿♥)',
+    '★~(◡﹏◕✿)',
+    '(╯3╰)',
+    'ｖ(⌒ｏ⌒)ｖ♪',
+    '＼(^。^ )',
+    'ﾍ(￣▽￣*)ﾉ',
+    '＼(^ω^＼)',
+    '',
+  ];
+
+  const hearts = [
+    '❤️',
+    '🧡',
+    '💛',
+    '💚',
+    '💙',
+    '💜',
+    '🤎',
+    '🤍',
+    '💕',
+    '💞',
+    '💓',
+    '💗',
+    '💖',
+    '💘',
+    '💝',
+    '💟',
+    '🥰',
+  ];
+
+  user
+    .send(
+      format(
+        `Thank you so so much for voting for me! **${Random.pick(faces)}** ${Random.pick(hearts)}`,
+        `You have now voted for me **${profile.votes.count}** times.`,
+        `I'm giving you **${amount}${INK_EMOJI}** for this!`,
+        `You now have **${newBalance}${INK_EMOJI}**.`,
+      ),
+    )
+    // Could not dm user. Sad but ok.
+    .catch(() => {});
 };
