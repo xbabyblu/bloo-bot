f4281339888380eaaef73d967e2039409af3fd48
Joaquim <joaquimmy@yahoo.com>
2020-01-31 13:02:23 -0300

    Vote command now tells if you can vote yet

--- src/commands/info/voting.js
+++ src/commands/info/voting.js
@@ -9,16 +9,22 @@ module.exports = new Command({
     const votesThisMonth = call.profile.getVoteCountThisMonth();
     const votesAllTime = call.profile.votes.count;
 
+    const timeDifference = Date.now() - call.profile.votes.time.getTime();
+    const time12hours = 12 * 60 * 60 * 1000;
+
     this.send(
       "I'm so **happy** you want to vote for me! I promise I'll remember this~ 💙",
-      '- You can vote for me every 12 hours. c:',
+      '- You can vote for me every __12 hours__. c:',
       '- During weekends voting gives you __double rewards__!',
       '- This month you have' +
         (votesThisMonth
           ? ` voted **${votesThisMonth}** time${votesThisMonth === 1 ? '' : 's'}.`
           : "n't voted yet this month."),
       '- You have' +
-        (votesAllTime ? ` voted **${votesAllTime}** time${votesAllTime === 1 ? '' : 's'} overall :D` : ' never voted for me :c.'),
+        (votesAllTime
+          ? ` voted **${votesAllTime}** time${votesAllTime === 1 ? '' : 's'} overall :D`
+          : ' never voted for me :c.'),
+      timeDifference > time12hours ? ':green_heart: Please vote for me again using the link bellow.' : ':white_heart: Please vote for me again when the 12 hours are up.',
       'https://top.gg/bot/643338599281983501/vote',
     );
   },
